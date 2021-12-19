8a8c5e6487d0da2dcceef9417305fc8fd0014380
alxtpl <alxtpl@gmail.com>
2021-12-19 13:47:32 +0200

    adding task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let btnNode = document.querySelector('.change-color')
+let colorNode = document.querySelector('.color')
+let bodyNode = document.querySelector('body')
+btnNode.addEventListener('click', onClickFunc);
+
+function onClickFunc(event) {
+    colorNode.textContent = getRandomHexColor()
+    bodyNode.style.backgroundColor = getRandomHexColor()
 }
