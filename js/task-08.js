5a0286f0f0206d2e9eabac075fdb0a3d90cf47c2
alxtpl <alxtpl@gmail.com>
2021-12-19 13:35:07 +0200

    adding task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+let inputFormNode = document.querySelector('.login-form')
 
+
+inputFormNode.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget)
+    const formElements = event.currentTarget.elements
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    if (mail && password !== '') {
+        inputFormNode.reset();
+        const formDataElements = { mail, password };
+        console.log('formDataElements:', formDataElements);
+    } else {
+        alert(`Fill in the empty fields!!!`)
+    }
+}
+// formData.forEach((value, name) => {
+//     value === '' ?
+//         alert(`Fill the ${name} field!!!`) : true
+// })
